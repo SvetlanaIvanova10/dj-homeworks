15915ae3042dbeb88a56d222a32acd8aa5ac275b
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-05-10 19:38:35 +0300

    table inflation

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+import csv
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
-
-    # чтение csv-файла и заполнение контекста
-    context = {}
-
+    list_inflation = []
+    list_context_year_and_summa = []
+    with open('inflation_russia.csv', encoding='utf-8', newline='\n') as csvfile:
+        csvfile.readline()
+        inflation = csv.reader(csvfile, delimiter=';')
+        for row in inflation:
+            list_month = []
+            list_month.append(row[0])
+            for elem_row in row[1:-1]:
+                if elem_row != '':
+                    str_to_float = float(elem_row)
+                    list_month.append(str_to_float)
+                else:
+                    list_month.append('-')
+            list_month.append(row[-1])
+            list_inflation.append(list_month)
     return render(request, template_name,
-                  context)
+                  context ={'table_inflation':list_inflation})
