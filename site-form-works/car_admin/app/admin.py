bb8edca727bba6264c584f0d998ca42f5ee32fbb
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-05-31 18:37:24 +0300

    adminka

--- site-form-works/car_admin/app/admin.py
+++ site-form-works/car_admin/app/admin.py
@@ -5,12 +5,18 @@ from .forms import ReviewAdminForm
 
 
 class CarAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('brand', 'model', 'review_count')
+    list_filter = ('brand', 'model')
+    ordering = ('brand', 'model')
+    search_fields = ['brand', 'model']
 
 
 class ReviewAdmin(admin.ModelAdmin):
     form = ReviewAdminForm
-
+    list_display = ('car', 'title')
+    list_filter = ('car', 'title')
+    ordering = ('car','title')
+    search_fields = ('car__id','title')
 
 admin.site.register(Car, CarAdmin)
 admin.site.register(Review, ReviewAdmin)
