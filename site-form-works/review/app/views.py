d676b28b90d09e028ead8f4ac7ef66a3f449c3cb
SvetlanaIvanova10 <fascinatedsveta@gmail.com>
2020-06-02 16:40:09 +0300

    add review

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -19,15 +19,28 @@ def product_list_view(request):
 def product_view(request, pk):
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
+    reviews = Review.objects.filter(product=product)
+    list_reviews = request.session['reviewed_products']
+    if request.method == 'GET':
+            is_review_exist = False
+            form = ReviewForm()
 
-    form = ReviewForm
     if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = Review(text=form.cleaned_data['text'], product=product)
+            review.save()
+            request.session.modified = True
+            if pk not in list_reviews:
+                list_reviews.append(pk)
+            is_review_exist = True
+            form = None
 
     context = {
         'form': form,
-        'product': product
+        'product': product,
+        'reviews': reviews,
+        'is_review_exist': is_review_exist
     }
 
     return render(request, template, context)
